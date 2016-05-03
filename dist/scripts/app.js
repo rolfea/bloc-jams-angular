7d1f09d342f8d75306220c175b7ad18803c29c8b
Alex Rolfe <rolfepercussion@gmail.com>
2016-05-03 10:35:39 -0500

    Add collection state and update href links to sref routing

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,2 +1,31 @@
-angular.module('blocJams', []);
-// Here we are again.
+(function() {
+  function config($stateProvider, $locationProvider) {
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+    
+    $stateProvider
+      .state('landing', {
+        url: '/',
+        templateUrl: '/templates/landing.html'
+      })
+      .state('album', {
+        url: '/album',
+        templateUrl: '/templates/album.html'
+      })
+      .state('collection', {
+        url: '/collection',
+        templateUrl: '/templates/collection.html',
+      });
+    
+    
+  }
+  
+  angular
+    .module('blocJams', ['ui.router'])
+    .config(config);
+})();
+
+
