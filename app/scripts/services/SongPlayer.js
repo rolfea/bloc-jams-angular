0a6f619d5b0cc43ea769d7aaba273cec711f6f4f
Alex Rolfe <rolfepercussion@gmail.com>
2016-05-10 23:54:07 -0500

    Add pause button

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -5,28 +5,45 @@
     var currentSong = null;
     var currentBuzzObject = null;
 
-    SongPlayer.play = function(song) {
-      if (currentSong !== song) {
-        if (currentBuzzObject) {
-          currentBuzzObject.stop();
-          currentSong.playing = null;
-        }
-      } else if (currentSong === song) {
-        if (currentBuzzObject.isPaused()) {
-          currentBuzzObject.play();
-        }
-      }
+    /**
+    * @function setSong
+    * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+    * @param {Object} song
+    */
 
+    var setSong = function(song) {
+      if (currentBuzzObject) {
+        currentBuzzObject.stop();
+        currentSong.playing = null;
+      }
+      /**
+      * @desc Buzz object audio file
+      * @type {Object}
+      */
       currentBuzzObject = new buzz.sound(song.audioUrl, {
         formats: ['mp3'],
         preload: true
       });
 
       currentSong = song;
-
-      currentBuzzObject.play();
-      song.playing = true;
     };
+
+    SongPlayer.play = function(song) {
+      if (currentSong !== song) {
+        setSong(song);
+        currentBuzzObject.play();
+        song.playing = true;
+      } else if (currentSong === song) {
+        if (currentBuzzObject.isPaused()) {
+          currentBuzzObject.play();
+        }
+      }
+    };
+
+    SongPlayer.pause = function(song) {
+      currentBuzzObject.pause();
+      song.playing = false;
+    }
 
     return SongPlayer;
   }
